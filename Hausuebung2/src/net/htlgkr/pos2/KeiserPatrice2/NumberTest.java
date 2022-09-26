aee4851dc0b04a7c6632196d05ba09de3bb575df
HunterPat <KeiserP190271@sus.htl-grieskirchen.at>
2022-09-26 09:42:07 +0200

    Patrice Keiser

---
+++ Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/NumberTest.java
@@ -0,0 +1,5 @@
+package net.htlgkr.pos2.KeiserPatrice2;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
---
+++ Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/NumberTester.java
@@ -0,0 +1,44 @@
+package net.htlgkr.pos2.KeiserPatrice2;
+
+import com.sun.jdi.IntegerType;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.IllegalFormatCodePointException;
+import java.util.Scanner;
+
+public class NumberTester {
+    private String fileName;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromTester;
+
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTeseter) {
+        this.oddTester = oddTeseter;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester) {
+        this.palindromTester = palindromTester;
+    }
+
+    public void testFile() {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            if(scanner.hasNextLine() != false)
+            {
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
