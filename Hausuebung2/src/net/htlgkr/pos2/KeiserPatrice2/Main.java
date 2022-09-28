e79be8fe2bb7daff2a3664271f571d44ac3aec86
HunterPat <KeiserP190271@sus.htl-grieskirchen.at>
2022-09-28 19:48:58 +0200

    Patrice Keiser

--- Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/Main.java
+++ Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/Main.java
@@ -3,8 +3,17 @@ package net.htlgkr.pos2.KeiserPatrice2;
 public class Main {
 
     public static void main(String[] args) {
+        /*Number 1.
     HalloJavaMitForEach halloJavaMitForEach = new HalloJavaMitForEach();
     halloJavaMitForEach.forEach();
+    */
+        /*Number 2.
+        */
+        NumberTester numberTester = new NumberTester("C:\\Users\\Windows\\Documents\\Hausuebung2\\Hausuebung2\\src\\net\\htlgkr\\pos2\\KeiserPatrice2\\text.txt");
+        numberTester.testFile();
+
+
+
     //PFAD: "C:\Users\Windows\Documents\Hausuebung2\Hausuebung2\src\net\htlgkr\pos2\KeiserPatrice2\text.txt"
     }
 }
--- Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/NumberTester.java
+++ Hausuebung2/src/net/htlgkr/pos2/KeiserPatrice2/NumberTester.java
@@ -1,13 +1,10 @@
 package net.htlgkr.pos2.KeiserPatrice2;
 
-import com.sun.jdi.IntegerType;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
-public class NumberTester {
+public class NumberTester implements NumberTest{
     private String fileName;
     private NumberTest oddTester;
     private NumberTest primeTester;
@@ -26,7 +23,7 @@ public class NumberTester {
         this.primeTester = primeTester;
     }
 
-    public void setPalindromeTester(NumberTester palindromeTester) {
+    public void setPalindromeTester(NumberTest palindromeTester) {
         this.palindromTester = palindromTester;
     }
 
@@ -34,14 +31,66 @@ public class NumberTester {
         String line;
         try {
             Scanner scanner = new Scanner(new File(fileName));
-            while (scanner.hasNextLine())
-            {
-               line = scanner.nextLine();
-               String[] strings = line.split(" ");
-               if()
+            while (scanner.hasNextLine()) {
+
+                line = scanner.nextLine();
+                String[] strings = line.split(" ");
+                if (strings.length == 1) {
+
+                } else {
+                    testNumber(Integer.parseInt(strings[0] + strings[1]));
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean testNumber(int number) {
+        char[] numberChars = Integer.toString(number).toCharArray();
+
+        if (numberChars[0] == 3) {
+                setPalindromeTester((palT) -> {
+                    char[] chars = Integer.toString(palT).toCharArray();
+                    int counter = 0;
+                    for (int i = 0; i < chars.length; i++) {
+
+                        if (chars[i] == chars[chars.length - (i + 1)]) {
+                            counter++;
+                        }
+                    }
+                    if (counter == chars.length) {
+                        System.out.println("PALINDROME");
+                        return true;
+                    } else {
+                        System.out.println("NO PALINDROME");
+                        return false;
+                    }
+                });
+
+        } else if (numberChars[0] == 2) {
+            setPrimeTester((primeT) -> {
+                if ((primeT / 2) == 0 && (primeT / 3) == 0) {
+                    System.out.println("PRIME");
+                    return true;
+                } else {
+                    System.out.println("NO PRIME");
+                    return false;
+                }
+
+            });
+            System.out.println("PRIME");
+        } else if (numberChars[0] == 1) {
+            setOddEvenTester((oddT) -> {
+                if (oddT % 2 == 0) {
+                    System.out.println("EVEN");
+                    return true;
+                } else {
+                    System.out.println("ODD");
+                    return false;
+                }
+            });
         }
     }
 }
